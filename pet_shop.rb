c0f7c15d78de10d5e89944255011014aea1d947a
Ross Melville <rossmelville@hotmail.com>
2017-06-30 14:13:09 +0100

    Function for counting stock of pet shop coded and passed test

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,15 +1,27 @@
 def pet_shop_name(pet_shop)
-  return pet_shop[:name]
+  pet_shop[:name]
 end
 
 def total_cash(pet_shop)
-  return pet_shop[:admin][:total_cash]
+  pet_shop[:admin][:total_cash]
 end
 
 def add_or_remove_cash(pet_shop, cash_amount)
-  pet_shop[:admin][:total_cash] = pet_shop[:admin][:total_cash] + cash_amount
+  pet_shop[:admin][:total_cash] += cash_amount
 end
 
 def pets_sold(pet_shop)
-  return pet_shop[:admin][:pets_sold]
+  pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pets_shop, sold_pets)
+  pets_shop[:admin][:pets_sold] += sold_pets
+end
+
+def stock_count(pet_shop)
+  num_of_pets = 0
+  for pet in pet_shop[:pets] do 
+    num_of_pets += 1
+  end
+  return num_of_pets
 end
