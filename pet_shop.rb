1fe620df2922b132c646645dab7dc1d481220808
Ross Melville <rossmelville@hotmail.com>
2017-07-01 12:09:44 +0100

    Function for customer_pet_count coded and test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -53,3 +53,11 @@ end
 def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets].push(new_pet)
 end
+
+def customer_pet_count(customer)
+  pet_count = 0
+  for pet in customer[:pets]
+    pet_count += 1
+  end
+  return pet_count
+end
